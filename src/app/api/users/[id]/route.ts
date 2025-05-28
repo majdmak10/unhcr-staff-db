d073273b85faedba8e4691d73a0ba9f41aff5313
Majd Makdessi <majd.mak10@hotmail.com>
2025-05-28 15:28:07 +0300

    Fixing rout.ts

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectToDb } from "@/utils/connectToDb";
 import { User } from "@/models";
 
-// Correct type for context: { params: { id: string } }
 export async function GET(
   req: NextRequest,
   context: { params: { id: string } }
@@ -11,19 +10,19 @@ export async function GET(
     await connectToDb();
     const { id } = context.params;
 
-    const admin = await User.findById(id);
-    if (!admin) {
+    const user = await User.findById(id);
+    if (!user) {
       return NextResponse.json(
-        { success: false, message: "Admin not found" },
+        { success: false, message: "User not found" },
         { status: 404 }
       );
     }
 
-    return NextResponse.json({ success: true, data: admin }, { status: 200 });
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
   } catch (error) {
-    console.error("Error fetching admin:", error);
+    console.error("Fetch user error:", error);
     return NextResponse.json(
-      { success: false, message: "Failed to fetch admin" },
+      { success: false, message: "Server error" },
       { status: 500 }
     );
   }
